8e98c868b55fe20e062ebcd3e21e0deaae50db62
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2017-07-16 01:02:30 +0200

    Add first component

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 // Here is the starting point for your application code.
 
-// Small helpers you might want to keep
+//Small helpers you might want to keep
 import './helpers/context_menu.js';
 import './helpers/external_links.js';
 
 // All stuff below is just to show you how it works. You can delete all of it.
 import { remote } from 'electron';
 import jetpack from 'fs-jetpack';
-import { greet } from './hello_world/hello_world';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './ui/containers/Home.jsx';
+
 import env from './env';
 
 const app = remote.app;
@@ -23,8 +27,6 @@ const osMap = {
   linux: 'Linux',
 };
 
-document.querySelector('#greet').innerHTML = greet();
-document.querySelector('#os').innerHTML = osMap[process.platform];
-document.querySelector('#author').innerHTML = manifest.author;
-document.querySelector('#env').innerHTML = env.name;
-document.querySelector('#electron-version').innerHTML = process.versions.electron;
+window.onload = function(){
+  ReactDOM.render(<Home />, document.getElementById('app'));
+}
--- tasks/build_app.js
+++ tasks/build_app.js
@@ -4,19 +4,19 @@ const watch = require('gulp-watch');
 const batch = require('gulp-batch');
 const plumber = require('gulp-plumber');
 const jetpack = require('fs-jetpack');
-const bundle = require('./bundle');
+// const bundle = require('./bundle');
 const utils = require('./utils');
 
 const projectDir = jetpack;
 const srcDir = jetpack.cwd('./src');
 const destDir = jetpack.cwd('./app');
 
-gulp.task('bundle', () => {
-  return Promise.all([
-    // bundle(srcDir.path('background.js'), destDir.path('background.js')),
-    bundle(srcDir.path('app.js'), destDir.path('app.js')),
-  ]);
-});
+// gulp.task('bundle', () => {
+//   return Promise.all([
+//     // bundle(srcDir.path('background.js'), destDir.path('background.js')),
+//     // bundle(srcDir.path('app.js'), destDir.path('app.js')),
+//   ]);
+// });
 
 gulp.task('less', () => {
   return gulp.src(srcDir.path('stylesheets/main.less'))
@@ -48,4 +48,4 @@ gulp.task('watch', () => {
   }));
 });
 
-gulp.task('build', ['bundle', 'less', 'environment']);
+gulp.task('build', [/*'bundle', */'less', 'environment']);
